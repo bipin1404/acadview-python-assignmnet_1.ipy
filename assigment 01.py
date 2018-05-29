616cdae3f1d9b5e85af96000e7e3cbbae974af11
bipin1404 <38787912+bipin1404@users.noreply.github.com>
2018-05-29 17:02:20 +0530

    Add files via upload

---
+++ assigment 01.py
@@ -0,0 +1,19 @@
+##question 1
+list1=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+#original list
+print(list1)
+list2=list1[7:12]#positive slicing
+list3=list1[-9:-4]#negative slicing
+#updated list
+print(list2)
+print(list3)
+##question 2
+
+lis1=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+print(lis1[1::2])
+
+##question 3
+
+lis2=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+lis3=lis2[::4]
+print(lis3)
